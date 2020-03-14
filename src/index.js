63ae817c7267bac992730755a3b6471738eb1950
Enzo Esperguin <enzoesper@hotmail.com>
2020-03-14 19:45:15 -0300

    using the ApprovalCard component to wrap multiple contents attached to its props.children property (passed in the App component)

--- src/index.js
+++ src/index.js
@@ -7,25 +7,36 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        content={faker.hacker.phrase()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 4:00PM"
-        content={faker.hacker.phrase()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Joe"
-        timeAgo="Today at 3:00PM"
-        content={faker.hacker.phrase()}
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content={faker.hacker.phrase()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 4:00PM"
+          content={faker.hacker.phrase()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Joe"
+          timeAgo="Today at 3:00PM"
+          content={faker.hacker.phrase()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
