3112488f83a5b62a75cb07088d16333c80a06950
Enzo Esperguin <enzoesper@hotmail.com>
2020-03-14 19:27:10 -0300

    passing props to the CommentDetail components from the parent component (App)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
-  return(
+  return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        content={faker.hacker.phrase()}
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 4:00PM"
+        content={faker.hacker.phrase()}
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Joe"
+        timeAgo="Today at 3:00PM"
+        content={faker.hacker.phrase()}
+        avatar={faker.image.avatar()}
+      />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
